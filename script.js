195c40e3205a9eb258ca38676b56e3456c7fd842
Павел <90463886+PavelHuman@users.noreply.github.com>
2022-01-09 16:18:10 +0300

    QueAndAns

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log(1);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', ''); 
+
+const personelMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personelMovieDB.movies[a] = b;
+personelMovieDB.movies[c] = d;
+
+console. log(personelMovieDB);
+
+
+
