eae90ed006bbac44bd15fa2b8577f31f6422f4b5
Павел <90463886+PavelHuman@users.noreply.github.com>
2022-01-10 20:32:59 +0300

    add modules

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+ import lesson1 from './lesson1.js';
+ import lesson2 from './lesson2.js';
+ import lesson3 from './lesson3.js';
+
+ console.log('scripts/index.js');
+ 
+ // lesson1()
+
+ lesson2()
+ lesson3()
---
+++ scripts/lesson1.js
@@ -0,0 +1,3 @@
+export default () => {
+    console.log('lesson1.js')
+};
