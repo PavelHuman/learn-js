36539678327c90e4911add4fbdd37164abb4ad29
Павел <90463886+PavelHuman@users.noreply.github.com>
2022-01-11 20:46:28 +0300

    function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,11 @@
 import lesson1 from './lesson1.js';
 import lesson2 from './lesson2.js';
 import lesson3 from './lesson3.js';
+import newLesson from './new.js';
 
 console.log('scripts/index.js');
 
-lesson1();
+// lesson1();
 lesson2();
 lesson3();
+newLesson();
