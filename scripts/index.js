5fe60f760d9b99f290c5f0e4dc789fbd0b4b7a4f
Павел <90463886+PavelHuman@users.noreply.github.com>
2022-01-13 01:13:33 +0300

    Clean Code

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 import lesson1 from './lesson1.js';
 import lesson2 from './lesson2.js';
 import lesson3 from './lesson3.js';
-import newLesson from './new.js';
+import initMovieDB from './movie.js';
 
 console.log('scripts/index.js');
 
 // lesson1();
 lesson2();
 lesson3();
-newLesson();
+initMovieDB();
