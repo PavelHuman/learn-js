7c7c0cf4daa0ea8fb53a231bc9bf86c856bb5c8c
Павел <90463886+PavelHuman@users.noreply.github.com>
2022-01-31 17:09:37 +0300

    boolean types, strict operators

---
+++ book/chapter-21.js
@@ -0,0 +1,32 @@
+function chapter21() {
+  console.log('chapter-21');
+
+  let bool;
+
+  bool = Boolean(true);
+  bool = Boolean('hello');
+  bool = Boolean(new Boolean()); // Внедрение!
+  bool = Boolean('false'); // строка
+  bool = Boolean({});
+  bool = Boolean(3.14);
+  bool = Boolean(['a', 'b', 'c']);
+
+  const boolObject = new Boolean(false);
+
+  if (boolObject) {
+    console.log('Bool, you so crazy!!!');
+  }
+
+  function theSoiution(answer) {
+    if (answer === 42) {
+      console.log('You have nothing more to learn!');
+    }
+  }
+
+  theSoiution('42');
+
+  console.log(new String('A') == new String('A')); // false
+  console.log([1, 2, 3] == [1, 2, 3]); // false
+  console.log({ a: 1 } == { a: 1 }); // false
+}
+export default chapter21;
--- book/index.js
+++ book/index.js
@@ -3,7 +3,8 @@
 // import chapter17 from './chapter-17.js';
 // import chapter18 from './chapter-18.js';
 // import chapter19 from './chapter-19.js';
-import chapter20 from './chapter-20.js';
+// import chapter20 from './chapter-20.js';
+import chapter21 from './chapter-21.js';
 
 function book() {
   console.log('book');
@@ -12,6 +13,7 @@ function book() {
   // chapter17();
   // chapter18();
   // chapter19();
-  chapter20();
+  // chapter20();
+  chapter21();
 }
 export default book;
