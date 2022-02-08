4c47954101bc3b108adfe34dd708e0c65139d273
PavelHuman <90463886+PavelHuman@users.noreply.github.com>
2022-02-08 19:45:44 +0300

    while, console.log, html

--- head-first-js/chapter-1.js
+++ head-first-js/chapter-1.js
@@ -4,12 +4,12 @@ function chapter1() {
   const discount = 10;
   const total = price - (price * (discount / 100));
   if (total > 25) {
-    freeShipping();
+    // freeShipping();
   }
 
   let count = 10;
   while (count > 0) {
-    juggle();
+    // juggle();
     count -= 1;
   }
 
@@ -23,5 +23,71 @@ function chapter1() {
   const circleRadius = 20;
   const circleArea = Math.PI * (circleRadius * circleRadius);
   console.log(circleArea);
+
+  (9 / 5) * temp + 32; // 50
+
+  color == 'orange' // pink-false, orange-true.
+
+  name + ", " + "you've won!" // Martha you've won!
+
+  yourLevel > 5 // 2 - false, 5 - false, 7 - true.
+
+    (level * points) + bonus; // 153300
+
+  color != 'orange' // color = pink; - true
+
+  1000 + "108"; // "1000108"
+
+  let scoops = 5;
+  while (scoops > 0) {
+    document.write('Another scoop!');
+    scoops -= 1;
+  }
+  document.write("Life without ice cream isn't the same");
+
+  const scoops1 = 2;
+  if (scoops1 >= 3) {
+    alert('Eat faster, the ice cream is going to melt!');
+  } else if (scoops1 < 3) {
+    alert('Ice cream is running low!');
+  }
+
+  const scoops2 = 1;
+  if (scoops2 >= 5) {
+    alert('Eat faster, the ice cream is going to melt!');
+  } else if (scoops2 == 3) {
+    alert('Ice cream is running low!');
+  } else if (scoops2 == 2) {
+    alert('Going once!');
+  } else if (scoops2 == 1) {
+    alert('Going twice!');
+  } else if (scoops2 == 0) {
+    alert('Gone!');
+  } else {
+    alert('Still lots of ice cream left, come and get it.');
+  }
+
+  const name = 'Joe';
+  let i = 0;
+  while (i < 2) {
+    document.write('Happy Birthday to you.');
+    i += 1;
+  }
+  document.write('Happy Birthday dear " + name + ",');
+  document.write('Happy Birthday to you.');
+
+  const word = 'bottles';
+  let count1 = 99;
+  while (count1 > 0) {
+    console.log(`${count1} ${word} of beer on the wall`);
+    console.log(`${count1} ${word} of beer,`);
+    console.log('Take one down, pass it around,');
+    count1 -= 1;
+    if (count1 > 0) {
+      console.log(`${count1} ${word} of beer on the wall.`);
+    } else {
+      console.log(`No more ${word} of beer on the wall.`);
+    }
+  }
 }
 export default chapter1;
